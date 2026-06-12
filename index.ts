b272b256a47bb960d2ab9cb3342ce8f4c6113567
Ricky112-creator <rickywhite254@gmail.com>
2026-06-12 19:40:21 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,3 +1,43 @@
+// Auth
+export {
+  buildAuthorizationUrl,
+  buildSignUpUrl,
+  initiateLogin,
+  initiateSignUp,
+  parseCallbackParams,
+  validateCallback,
+  exchangeCodeForTokens,
+  refreshAccessToken,
+  handleOAuthCallback,
+  cleanupUrl,
+  OAuthError,
+  fetchAccounts,
+  getWebSocketOTP,
+  logout,
+  generateRandomBase64url,
+  sha256Base64url,
+  base64urlEncode,
+  storeCSRFToken,
+  getCSRFToken,
+  clearCSRFToken,
+  storeCodeVerifier,
+  getCodeVerifier,
+  clearCodeVerifier,
+  storeAuthInfo,
+  getAuthInfo,
+  clearAuthInfo,
+  storeDerivAccounts,
+  getDerivAccounts,
+  clearDerivAccounts,
+  setActiveLoginId,
+  getActiveLoginId,
+  setAccountType,
+  getAccountType,
+  clearAllAuthData,
+  parseReferralLink,
+} from './auth';
+
+// Types
 export type {
   AuthConfig,
   AuthInfo,
@@ -8,9 +48,6 @@ export type {
   AuthState,
   StoredCSRFToken,
   StoredCodeVerifier,
-} from './auth';
-
-export type {
   ActiveSymbol,
   Tick,
   TicksHistoryResponse,
@@ -22,4 +59,22 @@ export type {
   BuyResponse,
   BuyResult,
   ProposalParams,
-} from './trading';
+} from './types';
+
+// Config
+export { getAuthBaseUrl, getApiBaseUrl, getPublicWsUrl } from './config';
+
+// Utils
+export { pickDefaultSymbol } from './utils/pick-default-symbol';
+
+// WebSocket
+export { DerivWS } from './ws';
+
+// React Hooks
+export {
+  useDerivWS,
+  useActiveSymbols,
+  useTicks,
+  useProposal,
+  useBuy,
+} from './react';
